93776cb0b935932a05a43e83a1675e746a910de0
pattersonhn <pattersonhn01@gmail.com>
2023-06-11 13:02:55 -0400

    added switch cases

--- server.js
+++ server.js
@@ -11,3 +11,57 @@ const connection = mysql.createConnection({
   database: "employee_info_db"
 });
 
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+  
+    startScreen();
+    
+  });
+
+  function startScreen() {
+    inquirer
+      .prompt({
+        type: "list",
+        choices: [
+          "Add department",
+          "Add role",
+          "Add employee",
+          "View departments",
+          "View roles",
+          "View employees",
+          "Update employee role",
+          "Quit"
+        ],
+        message: "What would you like to do?",
+        name: "option"
+      })
+      .then(function(result) {
+        console.log("You entered: " + result.option);
+        switch (result.option) {
+            case "Add department":
+              addDepartment();
+              break;
+            case "Add role":
+              addRole();
+              break;
+            case "Add employee":
+              addEmployee();
+              break;
+            case "View departments":
+              viewDepartment();
+              break;
+            case "View roles":
+              viewRoles();
+              break;
+            case "View employees":
+              viewEmployees();
+              break;
+            case "Update employee role":
+              updateEmployee();
+              break;
+            default:
+              quit();
+          }
+        });
+    }
